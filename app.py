d58eb85a46d9cf493f502aa04ca9b49232d43b3b
liwentao <linwentra@foxmail.com>
2022-08-05 19:39:34 +0800

    add theme

--- app.py
+++ app.py
@@ -20,12 +20,43 @@ st.image(image1)
 
 #image2 = Image.open('v2-3420614c84c85bba28ec098d771fb27d_1440w.jpg')
 
+'''
 with st.sidebar:
     add_radio = st.radio(
         '你想要：',
         ("电子唱诗", "来点儿词汇")
     )
+'''
 
+lucky_num = st.slider('请选择你的幸运数字', min_value=0, max_value=100, value=50,
+                          step=1)
+
+crazy_level = st.slider('请制定酒神精神 [0 分清醒 - 100 分疯狂]', min_value=0.00, max_value=1.00, value=0.5,
+                          step=0.01)
+
+if st.button('我想写一首！'):
+    with st.spinner("生成中........"):
+        time.sleep(5)
+
+        st.balloons()
+
+        result = read_txt('词库/存档.txt')
+        text = random_gene(lucky_num, result)
+        text = list(filter(None, text))
+        count = 0
+
+        for i in text:
+        #st.success(text)
+        text = change_sentence(i, lucky_num+count, crazy_level)
+        st.markdown(text)
+        count = count + 1
+    else:
+        st.warning('酒神在沉默... ...')
+
+st.image(image2)
+
+
+'''
 if add_radio == "电子唱诗":
 
     lucky_num = st.slider('请选择你的幸运数字', min_value=0, max_value=100, value=50,
@@ -82,3 +113,4 @@ st.image(image2)
 
 
 #st.image(image2)
+'''
